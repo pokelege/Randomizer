cf792ed63ec641f8bef8488d20a289b0480c7395
ポケレゲミッシングナムバー <pokelegemissingno@gmail.com>
2014-08-26 12:24:26 -0600

    add vjoy disabler

---
+++ VJoyDisabler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VJoyDisabler
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			if(args.Length > 1)
+			{
+				ProcessStartInfo info = new ProcessStartInfo( args[0] );
+				info.WorkingDirectory = Path.GetDirectoryName( args[0] );
+				info.Arguments = "-enable 0";
+				info.CreateNoWindow = true;
+				Process.Start(info);
+
+				ProcessStartInfo info2 = new ProcessStartInfo( args[1] );
+				info.WorkingDirectory = Path.GetDirectoryName( args[1] );
+				info.CreateNoWindow = true;
+				Process.Start( info2 );
+			}
+		}
+	}
+}
